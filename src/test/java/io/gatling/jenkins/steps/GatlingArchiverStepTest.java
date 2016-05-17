ed9df4b54c36cee467b3a82e42cb2111e93f9df5
Chris Price <chris@puppetlabs.com>
2016-05-17 11:39:43 -0700

    Add skeleton test for new Gatling "step"
    This commit just adds a skeleton test that we will be able to use
    to test a new Gatling pipeline "step" when we add it.  The commit
    also adds dependencies on the Jenkins plugins that are required
    to get the test running.
    
    The Gatling step itself will be introduced in a subsequent commit.

---
+++ src/test/java/io/gatling/jenkins/steps/GatlingArchiverStepTest.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2011-2015 GatlingCorp (http://gatling.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gatling.jenkins.steps;
+
+import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.util.Arrays;
+
+public class GatlingArchiverStepTest extends Assert {
+    @Rule public JenkinsRule j = new JenkinsRule();
+
+    /**
+     * Test archiving of gatling reports
+     */
+    @Test
+    public void archive() throws Exception {
+        // job setup
+        WorkflowJob foo = j.jenkins.createProject(WorkflowJob.class, "foo");
+        foo.setDefinition(new CpsFlowDefinition(StringUtils.join(Arrays.asList(
+                "node {",
+                "  echo 'hi'",
+                "}"), "\n")));
+
+        // get the build going, and wait until workflow pauses
+        WorkflowRun b = j.assertBuildStatusSuccess(foo.scheduleBuild2(0).get());
+
+        assertEquals(true, true);
+    }
+}
+
